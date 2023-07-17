0fe534c16379e9d649e20a6cbf47beddaf8a83b3
shimesaba9 <hshs012@gmail.com>
2023-07-17 15:15:26 +0900

    openつけ忘れ

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,8 +1,12 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-class MyClass {
-    func test() {
+open class MyClass {
+    open func test() {
         print("Hello.")
+    }
+    
+    func test2() {
+        print("Hello2.")
     }
 }
